e876837725a002af239059ffc47f95dd35ddb444
Jeffrey Quan <jeffrey_quan@mckinsey.com>
2024-11-20 18:13:49 +1100

    feat: add safeguard

--- ingestion.py
+++ ingestion.py
@@ -8,14 +8,18 @@ from langchain_text_splitters import CharacterTextSplitter
 
 load_dotenv()
 
-# Load the document
-loader = PyPDFLoader("data/impact_of_generativeAI.pdf")
-document = loader.load()
+if __name__ == "__main__":
+    print("ingesting data...")
+    
+    # load pdf document
+    loader = PyPDFLoader("data/impact_of_generativeAI.pdf")
+    document = loader.load()
+    
+    # split entire documents into chunks  
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+    texts = text_splitter.split_documents(document)
+    print(f"created {len(texts)} chunks")
 
-# Split the document into chunks
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
-texts = text_splitter.split_documents(document)
-print(f"created {len(texts)} chunks")
-
-embeddings = OpenAIEmbeddings(openai_api_type=os.environ.get("OPENAI_API_KEY"))
-PineconeVectorStore.from_documents(texts, embeddings, index_name=os.environ.get("INDEX_NAME"))
+    # create vector embeddings and save it in pinecone database
+    embeddings = OpenAIEmbeddings(openai_api_type=os.environ.get("OPENAI_API_KEY"))
+    PineconeVectorStore.from_documents(texts, embeddings, index_name=os.environ.get("INDEX_NAME"))
